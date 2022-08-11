6c3356685ad18cdc196d38950cef6f60feef04ab
chico <chico.zimcolor@gmail.com>
2022-08-11 17:30:24 -0300

    setup.py replaces project names within preset files

--- setup.py
+++ setup.py
@@ -43,9 +43,19 @@ class Project:
         return authors.split(',')
 
 
-@dataclass(frozen=True)
 class File:
-    path: Path
+    __path: Path
+
+    def __init__(self, path: str) -> None:
+        self.__path = Path(path)
+
+    @property
+    def path(self) -> Path:
+        return self.__path
+
+    @property
+    def name(self) -> str:
+        return self.path.name
 
     @property
     def content(self) -> str:
@@ -61,17 +71,53 @@ class File:
             file.write(content)
 
 
+class DefaultFile:
+    __OLD_PROJECT_NAME = 'djocketry-boiler'
+    __new_project_name: str
+    __expected_project_name_mentions: int
+    __file: File
+
+    def __init__(self, path: str, expected_project_name_mentions: int) -> None:
+        self.__file = File(path)
+        self.__expected_project_name_mentions = expected_project_name_mentions
+
+    @classmethod
+    def set_new_project_name(cls, name: str) -> None:
+        cls.__new_project_name = name
+
+    def replace_project_name(self) -> None:
+        if self.__replacement_allowed():
+            self.__file.replace(old=self.__OLD_PROJECT_NAME, new=self.__new_project_name)
+            print(f'{self.__file.name} edited')
+
+    def __replacement_allowed(self) -> bool:
+        old_name_mentions = self.__count_old_project_name_mentions()
+        if old_name_mentions != self.__expected_project_name_mentions:
+            yes, no = 'y', 'n'
+            message = f'{self.__file.name} should mention {self.__OLD_PROJECT_NAME} {self.__expected_project_name_mentions} times,\n' \
+                      f'but only {old_name_mentions} were found.\n' \
+                      f'This can cause issues, edit anyway? ({yes}/{no}) '
+            answer = ''
+            while answer not in [yes, no]:
+                answer = input(message).lower()
+            if answer is no:
+                return False
+        return True
+
+    def __count_old_project_name_mentions(self) -> int:
+        return len(re.findall(rf'{self.__OLD_PROJECT_NAME}', self.__file.content))
+
+
 class PyprojectToml:
     __FILE_NAME = 'pyproject.toml'
     __file: File
 
     def __init__(self) -> None:
-        file_path = Path(__file__).parent.joinpath(self.__FILE_NAME)
-        self.__file = File(file_path)
+        self.__file = File(self.__FILE_NAME)
 
     @property
     def file_name(self) -> str:
-        return self.__FILE_NAME
+        return self.__file.name
 
     def replace_attributes(self, *attributes: Tuple[str, str] or Tuple[str, List[str]]) -> None:
         [self.replace_attribute(*attribute) for attribute in attributes]
@@ -99,20 +145,34 @@ class PyprojectToml:
 
 
 class SetUp:
-    def execute(self) -> None:
-        project = Project.from_input()
-        self.__edit_pyproject_toml(project)
+    __project: Project
 
-    @staticmethod
-    def __edit_pyproject_toml(project: Project) -> None:
+    def execute(self) -> None:
+        self.__project = Project.from_input()
+        self.__edit_default_files()
+        self.__edit_pyproject_toml()
+
+    def __edit_pyproject_toml(self) -> None:
         pyproject_toml = PyprojectToml()
         pyproject_toml.replace_attributes(
-            ('name', project.name),
-            ('version', project.version),
-            ('description', project.description),
-            ('authors', project.authors)
+            ('name', self.__project.name),
+            ('version', self.__project.version),
+            ('description', self.__project.description),
+            ('authors', self.__project.authors)
         )
         print(f'{pyproject_toml.file_name} edited')
+
+    def __edit_default_files(self,) -> None:
+        DefaultFile.set_new_project_name(self.__project.name)
+        files = [
+            DefaultFile(path='manage.py', expected_project_name_mentions=1),
+            DefaultFile('docker-compose.yml', 3),
+            DefaultFile('djocketry-boiler/asgi.py', 2),
+            DefaultFile('djocketry-boiler/settings.py', 3),
+            DefaultFile('djocketry-boiler/urls.py', 1),
+            DefaultFile('djocketry-boiler/wsgi.py', 2)
+        ]
+        [file.replace_project_name() for file in files]
 
 
 SetUp().execute()
